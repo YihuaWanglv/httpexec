21b538dbe66e2078dae4b23d7dce98615649770e
yihuawanglv <wanglvyihua@gmail.com>
2018-09-10 01:17:18 +0800

    project init

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get('/cmd', (req, res) => {
 	res.send('exec ok!')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3002, () => console.log('Example app listening on port 3000!'))
