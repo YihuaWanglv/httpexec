fa7a22ee71d2ce39d473dd838c7fedd6fc27bc49
yihuawanglv <wanglvyihua@gmail.com>
2018-09-10 01:05:48 +0800

    project init

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/cmd', (req, res) => {
-	shell.exec("/d/datas/temp/shelltest.sh");
+	shell.exec("/home/app/build/update-blog-iyihua.sh");
 	res.send('exec ok!')
 })
 
